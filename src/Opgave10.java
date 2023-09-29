60f3755b51bb7749064cf2f4d6839993443570fc
sorensuper10 <soren_super@hotmail.com>
2023-09-29 10:31:58 +0200

    Updates

--- src/Opgave10.java
+++ src/Opgave10.java
@@ -2,13 +2,15 @@ public class Opgave10 {
     public static void main(String[] args) {
 
         int talArray[] = {13,10,7,5,60,88,0,17,99,1};
-        int k = 5;
-        System.out.println(k+" er på index " + linearSearch(talArray,k));
+        int k = 88;
+        //System.out.println(k+" er på index " + linearSearch(talArray,k));
         selectionSort(talArray);
-        for (int i = 0; i < 10 ; i++) {
-            System.out.print(talArray[i] + " ");
+        //for (int i = 0; i < 10 ; i++) {
+          //  System.out.print(talArray[i] + " ");
+
+        System.out.println(k + " er på index " + binarySearch(talArray,k));
         }
-    }
+
         public static int linearSearch(int[] list, int key) {
             for (int i = 0; i < list.length; i++) {
                 if (key == list[i])
@@ -17,6 +19,22 @@ public class Opgave10 {
             return -1;
         }
 
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            if (key < list[mid])
+                high = mid - 1;
+            else if (key == list[mid])
+                return mid;
+            else
+                low = mid + 1;
+        }
+
+        return -low - 1; // Now high < low
+    }
     public static void selectionSort(int[] list) {
         for (int i = 0; i < list.length - 1; i++) {
             // Find the minimum in the list[i..list.length-1]
